a1e5e2a895b39770347f5feb98314407580125b6
azmarifdev <azmarif.dev@gmail.com>
2023-11-01 01:55:57 +0600

    commi

--- blog-site-client/src/component/Banner.jsx
+++ blog-site-client/src/component/Banner.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-            Banner
+        <div className="px-4 py-32 bg-black mx-auto">
+            <div className="text-white text-center">
+                <h1 className="text-5xl lg:text-7xl leading-snug font-bold mb-5">
+                    Welcome to Our Blog
+                </h1>
+                <p className="lg:w-3/5 text-gray-100 mx-auto mb-5 font-primary">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Sapiente ipsa repellendus aperiam. Distinctio praesentium
+                    exercitationem dicta, veritatis dolore error minus
+                </p>
+            </div>
         </div>
     );
-};
+};           
 
 export default Banner;
