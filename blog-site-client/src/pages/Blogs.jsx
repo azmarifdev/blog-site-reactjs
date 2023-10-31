d75d85d334d162adc85fc6a49aa9c6b0da3fdda8
azmarifdev <azmarif.dev@gmail.com>
2023-11-01 02:30:41 +0600

    commi

--- blog-site-client/src/pages/Blogs.jsx
+++ blog-site-client/src/pages/Blogs.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
-  )
+      <>
+          <div className="px-4 py-32 bg-black mx-auto">
+              <div className="text-white text-center">
+                  <h1 className="text-5xl lg:text-7xl leading-snug font-bold mb-5">
+                      Welcome to Our Blog
+                  </h1>
+              </div>
+          </div>
+      </>
+  );
 }
 
 export default Blogs
